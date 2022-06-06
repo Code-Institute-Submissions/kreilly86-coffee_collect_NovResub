aaa0a5ff3aaa72dcef544bcaab31425f9e451b6c
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-06-06 21:19:26 +0000

    Update admin view list with filters to search posts

--- coffee_beans/admin.py
+++ coffee_beans/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('producer', 'slug', 'region',)
+    search_fields = ['producer', 'flavournotes']
+    list_filter = ('producer','region', 'variety','process')
+    prepopulated_fields = {'slug': ('producer',)}
