e6e5e2942b14d817aef8631fc03a935491353a48
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-09-18 14:34:29 +0000

    Add to Coffee Addition class

--- coffee_beans/views.py
+++ coffee_beans/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View
+from django.views import View
 from django.http import HttpResponseRedirect
 from .models import Coffee
 from .forms import CoffeeEntry
@@ -21,44 +21,34 @@ def coffees(request):
     coffees_list = Coffee.objects.all()
     context = {
         'coffees': coffees_list,
-        'coffee_add': False,
     }
     return render(request, 'coffees.html', context)
-
-
-class CoffeeList(generic.ListView):
-    model = Coffee
-    template_name = 'coffees.html'
-    paginate_by = 6
-    queryset = Coffee.objects.all()
 
 
 class CoffeeLike(View):
 
     def post(self, request, slug, *args, **kwargs):
         coffee = get_object_or_404(Coffee, slug=slug)
-        liked = False
         if coffee.likes.filter(id=request.user.id).exists():
-            liked = True
             coffee.likes.remove(request.user)
         else:
             coffee.likes.add(request.user)
 
-        context = {
-            "coffee": coffee,
-            "liked": liked,
-        }
-        return HttpResponseRedirect(reverse('coffees', context))
+        return HttpResponseRedirect(reverse('coffees'))
 
 
 class CoffeeAddition(View):
 
     def post(self, request, slug, *args, **kwargs):
-        coffee = get_object_or_404(Coffee, slug=slug)
+
+        def save(self, *args, **kwargs): 
+        if not self.slug: 
+            self.slug = slugify(self.title) 
+        return super().save(*args, **kwargs)
+        
         coffee_entry = CoffeeEntry(data=request.POST) 
         if coffee_entry.is_valid():
             coffee = coffee_entry.save()
-            coffee.save()
         else:
             coffee_entry = CoffeeEntry()
 
